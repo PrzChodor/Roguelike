e337378bf967e0d8183da52e2053055358fac0dc
Przemysław Chodór <prz.chodor@gmail.com>
2020-12-02 10:58:34 +0100

    Fixed NavMap

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -9,7 +9,20 @@ public class CameraController : MonoBehaviour
     public float smoothTime = 0.3f;
     private Vector3 velocity = Vector3.zero;
 
-    void Update()
+    private void Start()
+    {
+        var screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+        var mouse = Mouse.current.position.ReadValue() - screenCenter;
+        mouse.x = Mathf.Clamp(mouse.x / screenCenter.x, -1, 1);
+        mouse.y = Mathf.Clamp(mouse.y / screenCenter.y, -1, 1);
+
+        mouse *= 2;
+
+        var target = new Vector3(player.position.x + mouse.x, player.position.y + mouse.y, -10);
+        Camera.main.transform.position = target;
+    }
+
+    private void Update()
     {
         var screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
         var mouse = Mouse.current.position.ReadValue() - screenCenter;
