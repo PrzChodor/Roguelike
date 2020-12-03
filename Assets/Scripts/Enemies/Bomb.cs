535a542b9748f199e5ec9c741a8d00cafe58965f
Przemysław Chodór <prz.chodor@gmail.com>
2020-12-03 22:09:01 +0100

    Add Field of View to the player

--- Assets/Scripts/Enemies/Bomb.cs
+++ Assets/Scripts/Enemies/Bomb.cs
@@ -42,6 +42,7 @@ public class Bomb : Enemy
         enemy.constraints = RigidbodyConstraints2D.FreezeAll;
         animator.SetTrigger("Explode");
         Deactivate();
+        dead = true;
     }
 
     public override void OnDeath()
