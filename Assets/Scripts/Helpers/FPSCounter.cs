ed37a4fe0a6abc285b8d96ec9c6d1dcbb713e278
Przemysław Chodór <prz.chodor@gmail.com>
2020-12-06 22:23:38 +0100

    Add font and fps counter

---
+++ Assets/Scripts/Helpers/FPSCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    private int frames = 0;
+
+    private void Start()
+    {
+        StartCoroutine(OnEverySecond());
+    }
+
+    void Update()
+    {
+        frames++;
+    }
+
+    IEnumerator OnEverySecond()
+    {
+        while(true)
+        {
+            yield return new WaitForSecondsRealtime(1);
+            text.text = $"{frames}fps";
+            frames = 0;
+        }
+    }
+}
